666766e77efca1f52cf94f17658addf8e241c17e
estefaneluz <estefanecortes@gmail.com>
2021-06-29 21:38:26 -0300

    estrutura inicial

--- classe-01/src/App.js
+++ classe-01/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [quantidade, setQuantidade] = useState();
+  const cardapio = [
+  {
+    nome: "Moqueca",
+    preco: 73.25
+  }, 
+  {
+    nome: "Dobradinha",
+    preco: 25
+  },
+  {
+    nome: "Feijoada",
+    preco: 20
+  }]
+
   return (
     <div className="App">
-      
+      <h1>Cardápio</h1>
+      <ul>
+
+      </ul>
     </div>
   );
 }
