9477a6b021deca2a7a4c8586dd092b9ab2b0fe92
estefaneluz <estefanecortes@gmail.com>
2021-07-01 20:39:54 -0300

    add codigo do exercicio

--- classe-04/contador/src/App.js
+++ classe-04/contador/src/App.js
@@ -1,9 +1,43 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  const [delaySegundos, setDelaySegundos] = useState(1);
 
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setCount((c) => c + step),
+      delaySegundos * 1000
+    );
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <label>
+        Passo:
+        <br />
+        <input
+          type="number"
+          value={step}
+          onChange={(ev) => setStep(ev.target.valueAsNumber)}
+          min={0}
+        />
+      </label>
+      <label>
+        Delay:
+        <br />
+        <input
+          type="number"
+          value={delaySegundos}
+          onChange={(ev) => setDelaySegundos(ev.target.valueAsNumber)}
+          min={1}
+        />
+      </label>
     </div>
   );
 }
