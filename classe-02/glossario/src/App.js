97e22dcb5b3013216e9fe9ce1dbfe1915c4a9d87
estefaneluz <estefanecortes@gmail.com>
2021-07-01 18:57:51 -0300

    add codigo do exercicio

--- classe-02/glossario/src/App.js
+++ classe-02/glossario/src/App.js
@@ -1,23 +1,46 @@
 import './App.css';
 
+const glossario = [
+  {
+    termo: "HTML",
+    definicao:
+      "Linguagem de marcação responsável por definir a estrutura de uma página web"
+  },
+  {
+    termo: "CSS",
+    definicao: "Linguagem responsável por definir o estilo de uma página web"
+  },
+  {
+    termo: "Javascript",
+    definicao:
+      "Linguagem de programação responsável por ditar o comportamento de uma página web"
+  }
+];
+
 function App() {
+  const [mostrando, setMostrando] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h1>Meu app</h1>
+          <button>{mostrando ? "Fechar" : "Abrir"} menu</button>
+
+          {mostrando && (
+              <div className="remover-div">
+                  <button>Adicionar entrada</button>
+                  <button>Remover entrada</button>
+              </div>
+          )}
+
+          <dl>
+              {glossario.map(entrada => (
+                  <div className="remover-div">
+                      <dt>{entrada.termo}</dt>
+                      <dd>{entrada.definicao}</dd>
+                  </div>
+              ))}
+          </dl>
+      </div>
   );
 }
 
